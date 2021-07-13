dc4679fb32f70dbd813fea56c1a16e89aff36d6a
ryukyucolin2 <cojorichardson@gmail.com>
2021-07-13 03:21:58 +0000

    add erb method to display finstagram post data

--- app/actions.rb
+++ app/actions.rb
@@ -8,7 +8,7 @@ end
 
 # Index Page
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -21,7 +21,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -34,7 +34,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -47,6 +47,8 @@ get '/' do
     }]
   }
 
-  [ finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin ].to_s
+  @finstagram_posts = [ @finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin ]
+
+  erb :index
 end
 
